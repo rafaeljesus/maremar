23c95a41624f66742086bf321af5d9f6bdadee5b
rafaeljesus <rafael_jesus86@hotmail.com>
2014-07-20 00:01:33 -0300

    cover vehicle update backend mocha

--- test/controllers/vehicle.js
+++ test/controllers/vehicle.js
@@ -6,7 +6,22 @@ var app = require('../../app')
 
 describe('Vehicle Controller', function() {
 
-  var imgPath = 'public/bower_components/flat-ui/images/exaple-image.jpg';
+  var vehicle
+  , imgPath = 'public/bower_components/flat-ui/images/exaple-image.jpg'
+  ;
+
+  beforeEach(function(done) {
+    var options = {
+      name: 'vehicle 1',
+      driver: 'driver 1',
+      capacity: 1
+    };
+    Vehicle.create(options, function(err, doc) {
+      if (err) return done(err);
+      vehicle = doc;
+      done();
+    });
+  });
 
   afterEach(function(done) {
     Vehicle.remove(function(err) {
@@ -16,22 +31,38 @@ describe('Vehicle Controller', function() {
   });
 
   it('when vehicle is valid then create', function(done) {
-    var vehicle = {
-      name: 'vehicle 1',
-      driver: 'driver 1',
-      capacity: 1
-    };
     request
       .post('/vehicles')
-      .field('vehicle[name]', vehicle.name)
-      .field('vehicle[driver]', vehicle.driver)
-      .field('vehicle[capacity]', vehicle.capacity)
-      .attach('photo', imgPath)
+      .field('name', vehicle.name)
+      .field('driver', vehicle.driver)
+      .field('capacity', vehicle.capacity)
+      .attach('file', imgPath)
+      .expect(201)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.body._id).to.not.equal(null);
+        expect(res.body.picture.filename).to.not.equal(null);
+        done();
+      });
+  });
+
+  it('when a existing vehicle is changed then update', function(done) {
+    var newCapacity = 5;
+    var options = {
+      vehicle: {
+        name: vehicle.name,
+        driver: vehicle.driver,
+        capacity: newCapacity
+      }
+    };
+    request
+      .put('/vehicles/' + vehicle._id)
+      .send(options)
       .expect(200)
       .end(function(err, res) {
         if (err) return done(err);
-        expect(res.body.name).to.equal(vehicle.name);
-        expect(res.body.photo).to.not.equal(null);
+        expect(res.body._id).to.not.equal(null);
+        expect(vehicle.capacity).to.not.equal(newCapacity);
         done();
       });
   });
