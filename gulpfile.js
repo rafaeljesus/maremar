470364078d3dd7aa3fadf759cef54aceead3d498
Rafael Jesus <rafael_jesus86@hotmail.com>
2014-11-11 23:42:26 -0200

    change codecoverage

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp')
-, concat = require('gulp-concat')
-, uglify = require('gulp-uglify')
-, minifyCSS = require('gulp-minify-css');
+  , concat = require('gulp-concat')
+  , uglify = require('gulp-uglify')
+  , minifyCSS = require('gulp-minify-css');
 
 var scripts = [];
 
